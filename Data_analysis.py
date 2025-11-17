4fd438c51ec6bca48a39fe7702819945453bc997
Se-Boruk <Seb.Boruk@gmail.com>
2025-11-17 20:37:23 +0100

    Data analysis
    histogram klas

---
+++ Data_analysis.py
@@ -0,0 +1,106 @@
+###################################################################
+# ( 0 ) Libs and dependencies 
+###################################################################
+
+#Basic libs
+import sys
+from sklearn.decomposition import PCA
+from sklearn.metrics import accuracy_score
+
+from Config import DATABASE_FOLDER, DATASET_PATH, DATASET_NAME
+from Config import TRAIN_SPLIT, VAL_SPLIT, TEST_SPLIT, RANDOM_STATE
+
+
+
+##########################################################################################
+#Move folder up to go to database folder to use manager from here
+sys.path.insert(0, DATABASE_FOLDER)
+from DataBase_Functions import Custom_DataSet_Manager, LabelEncoderDF, Prepare_data_from_features
+import Functions
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+import numpy as np
+import matplotlib.pyplot as plt
+
+###################################################################
+# ( 1 ) Loading data
+###################################################################
+
+#Data loading (Not stored in RAM but is accessed on demand)
+
+#Load manager and execute
+manager = Custom_DataSet_Manager(DataSet_path = DATASET_PATH,
+                                 train_split = TRAIN_SPLIT,
+                                 val_split = VAL_SPLIT,
+                                 test_split = TEST_SPLIT,
+                                 random_state = RANDOM_STATE
+                                 )
+
+#Download data if not present
+manager.download_database(DATASET_NAME)
+#Load dataset
+Train_set, Val_set, Test_set = manager.load_dataset_from_disk()
+
+
+
+###################################################################
+# ( 2 ) Extracting features from image
+###################################################################
+Train_features, Val_features, Test_features = manager.transform_to_features()
+
+
+#Drop nans values
+print("Dropping NaNs...")
+Train_features = Train_features.dropna().reset_index(drop=True)
+Val_features   =  Val_features.dropna().reset_index(drop=True)
+Test_features  = Test_features.dropna().reset_index(drop=True)
+
+print("Labeling...")
+#replacing labels with numbers
+Label_encoder = LabelEncoderDF()
+Train_features = Label_encoder.transform_df(Train_features)
+Val_features = Label_encoder.transform_df(Val_features)
+Test_features = Label_encoder.transform_df(Test_features)
+
+print("Transforming dataframe into numpy arrays")
+#Transforming features df into the x and y for some svm or decision tree
+x_train, y_train = Prepare_data_from_features(Train_features, label_col = "label")
+x_val, y_val = Prepare_data_from_features(Val_features, label_col = "label")
+x_test, y_test = Prepare_data_from_features(Test_features, label_col = "label")
+
+print("Performing PCA space reduction")
+#Reducing to PCA
+pca = PCA(n_components=0.9999)
+x_train_p = pca.fit_transform(x_train)
+x_val_p = pca.transform(x_val)
+x_test_p = pca.transform(x_test)
+
+
+
+####################
+#histogram of the sample distribution
+#####################
+
+unique, counts = np.unique(y_train, return_counts = True)
+normalized_counts = counts / counts.sum()
+
+plt.figure()
+plt.bar(unique, normalized_counts, width = 0.9, edgecolor = "black")
+plt.xlabel("Classes")
+plt.ylabel("Normalized occurence")
+
+plt.title("Histogram of classes in train dataset")
+plt.grid(axis = 'y', linestyle = '--', alpha = 0.7)
+
+
+
+
+
+
+
+
+
+
+
